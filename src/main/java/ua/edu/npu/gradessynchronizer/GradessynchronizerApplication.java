51dc33b564090ee27990cd9e9a22e37ce68da76d
Kyrylo Karpenko <karpenkokirillll55@gmail.com>
2022-11-10 20:24:44 +0200

    Project setup, add multiple databases support

---
+++ src/main/java/ua/edu/npu/gradessynchronizer/GradessynchronizerApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.npu.gradessynchronizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradessynchronizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradessynchronizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/edu/npu/gradessynchronizer/GradessynchronizerApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.npu.gradessynchronizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradessynchronizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
